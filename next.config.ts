e1d09a3478effb2619bb3624270a3b7722ef967a
Henio Pedro <hpssantana.cic@uesc.br>
2025-09-28 12:31:29 -0300

    Versão inicial do BrandBook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",          // isso substitui o antigo next export
+  basePath: "/biblia_da_marca", // nome do repositório no GitHub
+  images: { unoptimized: true }, // necessário para export estático
 };
 
-export default nextConfig;
+module.exports = nextConfig;
